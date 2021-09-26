9c996360762de475e3f4dcb4878ac63cf52fc512
Josephine Lee <lee.josie.y@gmail.com>
2021-09-26 15:29:12 -0400

    adding combined code

--- 05/names2.py
+++ 05/names2.py
@@ -1,1 +1,9 @@
 import random
+
+list1 = ["Rayat", "William", "Michelle", "Lucas", "Ivan"];
+list2 = ["Yoonah", "Joshua", "Alif", "Josephine", "Andrew"];
+
+def printName():
+    print("period 1: " + list1[random.randint(0, len(list1) - 1)])
+    print("period 2: " + list2[random.randint(0, len(list2) - 1)])
+printName()
