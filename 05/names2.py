6f913c3c10506afb866cc2bd675643a92efbb764
Josephine Lee <56737932+josephinelee1234@users.noreply.github.com>
2021-09-27 09:26:13 -0400

    Update names2.py

--- 05/names2.py
+++ 05/names2.py
@@ -1,13 +1,15 @@
 #pow-wow summary: we shared code and discussed how to combine/improve the code.
-#Disconcerns:none
+#Discoveries:none
 #Questions: none
 #Comments: helped me get familiar with Python again
 import random
 
-list1 = ["Rayat", "William", "Michelle", "Lucas", "Ivan"];
-list2 = ["Yoonah", "Joshua", "Alif", "Josephine", "Andrew"];
+students={
+'list1': ['Rayat', 'William', 'Michelle', 'Lucas', 'Ivan'],
+'list2': ['Yoonah', 'Joshua', 'Alif', 'Josephine', 'Andrew'],
+}
 
 def printName():
-    print("period 1: " + list1[random.randint(0, len(list1) - 1)])
-    print("period 2: " + list2[random.randint(0, len(list2) - 1)])
+    print("period 1: " + students['list1'][random.randint(0, len('list1') - 1)])
+    print("period 2: " + students['list2'][random.randint(0, len('list2') - 1)])
 printName()
