ba064f358d2059952a3f544484cb1193e3dacbd3
Josephine Lee <lee.josie.y@gmail.com>
2021-10-05 20:52:36 -0400

    last part of K10

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -5,11 +5,12 @@
 
 from flask import Flask
 from K06 import jobDecider
+from occupy_flask_st.K06 import getOccupations
 
 app = Flask(__name__)
 
 @app.route("/")
 def job_decider_web():
-    return jobDecider()
+    return "Hi-C: Yaying Liang Li, Andy Lin, Josephine Lee <br><br>"+getOccupations()+"<br><br>"+"Occupation Chosen:<br><br>"+jobDecider()
 
 app.run()
