c34494560ef1ee94107fac4c06cd752210bcde86
Josephine Lee <56737932+josephinelee1234@users.noreply.github.com>
2021-10-08 09:19:01 -0400

    app.py

---
+++ 13_tempwork/app.py
@@ -0,0 +1,41 @@
+# The Penguins- Qina Liu, Josephine Lee, Roshani Shrestha
+# SoftDev
+# K13 - Template for Success
+# 2021-10-08
+
+import random
+from flask import Flask
+app = Flask(__name__);
+
+@app.route("/")
+def hello_world():
+    file = open("occupations.csv");
+    lines = file.read().split("\n");
+    del lines[0]; #Remove "Job Class, Percentage" line
+    split = [];
+    for i in lines:
+        if "," in i:
+            #remove quotes, split string into job and %, then convert % to float
+            i = i.replace("\"","");
+            comma = i.rsplit(",",1);
+            comma[1] = float(comma[1]);
+            #add to necessary arrays
+            split.append(comma);
+
+    del split[len(split)-1]; # Remove "Total" as a job
+    dictionary = dict(split)
+    
+    html = "<h2> The Penguins- Qina Liu, Josephine Lee, Roshani Shrestha </h2><hr>";
+    
+    html += "Random Weighted Job: " + (random.choices(list(dictionary), weights=dictionary.values()))[0] + "<br><br><br>";
+    
+    html += "<table> <tr> <th> Job </th> <th> Percentage </th> </tr>";
+    for i in list(dictionary):
+        html += "<tr>";
+        html += ("<td>" + i + "&nbsp;&nbsp;&nbsp;</td>");
+        html += ("<td>" + str(dictionary[i]) + "% </td>");
+        html += "</tr>";
+        
+    return html;
+
+app.run();
