eb30a56ba81c5925d270c9b900aff757416f0257
Josephine Lee <56737932+josephinelee1234@users.noreply.github.com>
2021-10-21 15:10:48 -0400

    K16 classwork

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,7 +1,7 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev  
-#skeleton/stub :: SQLITE3 BASICS
-#Dec 2020 -- The Time of the Rona
+# Team Whales: Hebe Huang, Josephine Lee, Han Zhang
+# SoftDev
+# K16: All About Database
+# 2021-10-21
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -9,21 +9,28 @@ import csv       #facilitate CSV I/O
 
 DB_FILE="discobandit.db"
 
-db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+db = sqlite3.connect("Whales") #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
 #==========================================================
-
+c.execute("CREATE TABLE IF NOT EXISTS roster(name TEXT, age INTEGER, id INTEGER)") #creates table if one does not exist
 
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
+with open("students.csv", mode = 'r') as students:  #reads in csv file
+    file = csv.DictReader(students)
+    for lines in file:                              #iterates through csv rows
+        script = "INSERT INTO roster VALUES(\"" + lines['name'] + "\"," + lines['age'] + "," + lines['id'] + ")" #scripts 
+        c.execute(script)
 
-
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+c.execute("CREATE TABLE IF NOT EXISTS courses(code TEXT, mark INTEGER, id INTEGER)") #creates table if one does not exist
+with open("courses.csv", mode = 'r') as courses:  #reads in csv file
+    file = csv.DictReader(courses)
+    for lines in file:                              #iterates through csv rows
+        script = "INSERT INTO courses VALUES(\"" + lines['code'] + "\"," + lines['mark'] + "," + lines['id'] + ")" #scripts 
+        c.execute(script)
 
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
 
