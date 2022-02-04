24516ccf8cd72a2235d7d7fe6d4e44d9f98ef6f7
Josephine Lee <56737932+josephinelee1234@users.noreply.github.com>
2022-02-04 09:20:28 -0500

    fibfac js

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -11,3 +11,17 @@
 
 
 //Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
+
+function fact (n){
+    if (n == 0){
+      return 1;
+    }
+    return (n * (fact(n-1)));
+}
+
+function fib (n){
+    if (n <= 1){
+        return n;
+    }
+    return (fib(n-1) + fib(n-2));
+}
