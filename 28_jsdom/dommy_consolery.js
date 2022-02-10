aec3a323553549bcfdfa2005361fa918d785759a
Josephine Lee <56737932+josephinelee1234@users.noreply.github.com>
2022-02-09 20:57:30 -0500

    k28

---
+++ 28_jsdom/dommy_consolery.js
@@ -0,0 +1,97 @@
+/*
+    Team Sheeps :: Josephine Lee, Ivan Lam
+    SoftDev pd2
+    K28: DOMfoolery
+    2022-02-09
+*/
+
+//send diagnostic output to console
+//(Ctrl-Shift-K in Firefox to reveal console)
+console.log("AYO");
+
+var i = "hello";
+var j = 20;
+
+
+//assign an anonymous fxn to a var
+var f = function(x) {
+  var j=30;
+  return j+x;
+};
+
+
+//instantiate an object
+var o = { 'name' : 'Thluffy',
+          age : 15,
+          items : [10, 20, 30, 40],
+          morestuff : {a : 1, b : 'ayo'},
+          func : function(x) {
+            return x+30;
+          }
+        };
+
+
+var addItem = function(text) {
+  var list = document.getElementById("thelist");
+  var newitem = document.createElement("li");
+  newitem.innerHTML = text;
+  list.appendChild(newitem);
+};
+
+
+var removeItem = function(n) {
+  var listitems = document.getElementsByTagName('li');
+  listitems[n].remove();
+};
+
+
+var red = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    items[i].classList.add('red');
+  }
+};
+
+
+var stripe = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    if (i%2==0){
+      items[i].classList.add('red');
+    } else {
+      items[i].classList.add('blue');
+    }
+  }
+};
+
+function fact (n){
+    if (n == 0){
+      return 1;
+    }
+    return (n * (fact(n-1)));
+}
+
+function fib (n){
+    if (n <= 1){
+        return n;
+    }
+    return (fib(n-1) + fib(n-2));
+}
+
+function gcd (a, b) {
+    let c = min(a, b);
+    for (var i = c; i > 0; i--) {
+        if (a % i == 0 && b % i == 0) {
+            return i;
+        }
+    }
+}
+
+function min (a, b) {
+    if (a > b) {
+        return b;
+    } else {
+        return a;
+    }
+}
+
